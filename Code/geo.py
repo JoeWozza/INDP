7bbaa03ddd12318e2ba002213a9fa3d217704d16
Joe Wozniczka-Wells <Joe.WozniczkaWells@phe.gov.uk>
2021-12-19 10:56:23 +0000

    Loop that creates circle-based approximation of Derbyshire

--- Code/geo.py
+++ Code/geo.py
@@ -32,6 +32,7 @@ import numpy as np
 import json
 import geog
 from shapely.geometry import Point, mapping, Polygon, asPolygon
+from shapely.ops import cascaded_union
 import shapely
 import pandas as pd
 import folium
@@ -86,6 +87,9 @@ circle_perc = circle_poly.intersection(derbs_poly).area/circle_poly.area*100
 
 #%% Try to cover Derbyshire in circles
 ### S1
+
+# Create empty dataframe, to contain centroids and radiuses of circles
+df_derbs = pd.DataFrame(columns = ['lat','long','radius'])
 
 # Get Derbyshire polygon as list of coords
 def coord_lister(geom):
@@ -150,12 +154,17 @@ if derbs_cent.within(derbs_poly):
     circle_geoj_1 = folium.GeoJson(data=circle_poly_1,
                        style_function=lambda x: {'fillColor': 'blue'})
     
-    circle_geoj_1.add_to(m)
-
+    dict_derbs = {'lat': derbs_cent.y, 'long': derbs_cent.x, 'radius': radius-1}
+    df_derbs = df_derbs.append(dict_derbs, ignore_index = True)
+    
+    #circle_geoj_1.add_to(m)
+#m.save("mymap.html")
 #	3. Take a random point from the circumference of the first circle and check 
 # it is in the target UTLA.
-rand_point = Point(list(circle_poly_1.exterior.coords)[randrange(361)][0],
-                   list(circle_poly_1.exterior.coords)[randrange(361)][1])
+rand_no = randrange(361)    
+
+rand_point = Point(list(circle_poly_1.exterior.coords)[rand_no][0],
+                   list(circle_poly_1.exterior.coords)[rand_no][1])
 
 #		a. If it is not, choose another random point from the circ. of the first 
 #   circle and check it is in the target UTLA. Proceed to step 4 when a point 
@@ -163,8 +172,9 @@ rand_point = Point(list(circle_poly_1.exterior.coords)[randrange(361)][0],
 #   identified.
 while rand_point.within(derbs_poly) == False:
     print("Find a new random point that is in Derbyshire")
-    rand_point = Point(list(circle_poly_1.exterior.coords)[randrange(361)][0],
-                       list(circle_poly_1.exterior.coords)[randrange(361)][1])
+    rand_no = randrange(361)
+    rand_point = Point(list(circle_poly_1.exterior.coords)[rand_no][0],
+                       list(circle_poly_1.exterior.coords)[rand_no][1])
 
 #	4. Repeat step 2, using the point identified in step 3. Circle_perc should 
 # be updated to be the area covered by any of the circles.
@@ -183,20 +193,26 @@ while (circle_perc_tot >= 95) & (derbs_perc_tot < 98):
     circle_perc_tot = ((circle_derbs_area_tot + circle_derbs_area - 
                         circle_poly_1.intersection(circle_poly)
                        .intersection(derbs_poly).area)/
-        (circle_poly_1.area + circle_poly.area)*100)
+        (circle_poly_1.area + circle_poly.area - 
+         circle_poly_1.intersection(circle_poly).area)*100)
     print(radius)
-    # The two below aren't right - WORK OUT WHY
     print(derbs_perc_tot)
     print(circle_perc_tot)
+    dict_derbs = {'lat': rand_point.y, 'long': rand_point.x, 'radius': radius-1}
+    df_derbs = df_derbs.append(dict_derbs, ignore_index = True)
 
-derbs_perc_tot = draw_circle(rand_point,radius-1)[0]
-circle_poly_1 = draw_circle(rand_point,radius-1)[2]
+circle_derbs_area = draw_circle(rand_point,radius-1)[0]
+circle_poly_2 = draw_circle(rand_point,radius-1)[1]
+
+circles_comb_poly = cascaded_union([circle_poly_1,circle_poly_2])
     
-circle_geoj_1 = folium.GeoJson(data=circle_poly_1,
+circles_comb_geoj = folium.GeoJson(data=circles_comb_poly,
                    style_function=lambda x: {'fillColor': 'blue'})
 
-circle_geoj_1.add_to(m)
+circles_comb_geoj.add_to(m)
+m.save("mymap.html")
 
+circle_derbs_area_tot = (circles_comb_poly.intersection(derbs_poly).area)
 
 #	5. Repeat step 3, using points from the circle drawn in step 4 rather than the original circle.
 #	6. Repeat steps 4 and 5 until a circle with radius >= 1km cannot be drawn so circle_perc >= 95
@@ -205,11 +221,123 @@ circle_geoj_1.add_to(m)
 
 
 
+#%% Try to put it all into one step
+# Create empty dataframe, to contain centroids and radiuses of circles
+df_derbs = pd.DataFrame(columns = ['lat','long','radius'])
 
+# Get Derbyshire polygon as list of coords
+def coord_lister(geom):
+    coords = np.array(geom.exterior.coords)
+    return (coords)
 
+derbs = (utla_polygons[utla_polygons['CTYUA21NM']=='Derbyshire'].geometry.
+         apply(coord_lister).iloc[0])
 
+derbs_poly = Polygon([(d[0],d[1]) for d in derbs])
 
+derbs_geoj = folium.GeoJson(data=derbs_poly,
+                       style_function=lambda x: {'fillColor': 'orange'})
 
+m = folium.Map(location=[utla_polygons[utla_polygons['CTYUA21NM']=='Derbyshire'].LAT,
+               utla_polygons[utla_polygons['CTYUA21NM']=='Derbyshire'].LONG],
+               tiles = 'CartoDB positron')
+derbs_geoj.add_to(m)
 
+def draw_circle(point,radius):
+    # Draw approximation of a circle using geopy.destination
+    d_obj_v = geopy.distance.distance(kilometers=radius)
+    
+    circle_array = []
+    for b in range(0,360):
+        coords = ((d_obj_v.destination(point = geopy.Point(point.y, point.x), bearing = b).latitude),
+                  (d_obj_v.destination(point = geopy.Point(point.y, point.x), bearing = b).longitude))
+        circle_array.append(coords)
+        
+    circle_poly = Polygon([(c[1],c[0]) for c in circle_array])
+    
+    # Area of circle that is in Derbyshire
+    circle_derbs_area = circle_poly.intersection(derbs_poly).area
+    
+    return circle_derbs_area, circle_poly
 
+# 1. Check centroid is in target UTLA (may be that in some cases it is in 
+# another UTLA that is entirely within the target UTLA or that an irregular 
+# (e.g. concave) shape means it is in a different UTLA)
+derbs_cent = Point(utla_polygons[utla_polygons['CTYUA21NM']=='Derbyshire'].LONG,
+                   utla_polygons[utla_polygons['CTYUA21NM']=='Derbyshire'].LAT)
+
+if derbs_cent.within(derbs_poly):
+    #	2. Try to get the biggest circle possible, where the centre is the UTLA 
+    # centroid, that contains no more than X(start with 5)% of other UTLAs: 
+    # start with 1km radius. If circle_perc >= 95, add 1km to radius until 
+    # circle_perc < 95, then use circle with radius = radius - 1.
+    radius = 1
+    circle_poly = draw_circle(derbs_cent,radius)[1]
+    derbs_perc = draw_circle(derbs_cent,radius)[0]/derbs_poly.area*100
+    circle_perc = draw_circle(derbs_cent,radius)[0]/circle_poly.area*100
+    
+    while circle_perc >= 95:
+        radius = radius + 1
+        circle_poly = draw_circle(derbs_cent,radius)[1]
+        derbs_perc = draw_circle(derbs_cent,radius)[0]/derbs_poly.area*100
+        circle_perc = draw_circle(derbs_cent,radius)[0]/circle_poly.area*100
+    
+    all_poly = draw_circle(derbs_cent,radius-1)[1]
+    
+    dict_derbs = {'lat': derbs_cent.y, 'long': derbs_cent.x, 'radius': radius-1}
+    df_derbs = df_derbs.append(dict_derbs, ignore_index = True)
+    
+derbs_perc_tot = all_poly.intersection(derbs_poly).area/derbs_poly.area*100
+
+while derbs_perc_tot < 98:
+    #	3. Take a random point from the circumference of the first circle and check 
+    # it is in the target UTLA.
+    rand_no = randrange(len(all_poly.exterior.coords))
+    
+    rand_point = Point(list(all_poly.exterior.coords)[rand_no][0],
+                       list(all_poly.exterior.coords)[rand_no][1])
+    
+    #		a. If it is not, choose another random point from the circ. of the first 
+    #   circle and check it is in the target UTLA. Proceed to step 4 when a point 
+    #   on the circ. of the first circle that is in the target UTLA has been 
+    #   identified.
+    while rand_point.within(derbs_poly) == False:
+        print("Find a new random point that is in Derbyshire")
+        rand_no = randrange(len(all_poly.exterior.coords))
+        rand_point = Point(list(all_poly.exterior.coords)[rand_no][0],
+                           list(all_poly.exterior.coords)[rand_no][1])
+    
+    #	4. Repeat step 2, using the point identified in step 3. Circle_perc should 
+    # be updated to be the area covered by any of the circles.
+    circle_derbs_area_tot = all_poly.intersection(derbs_poly).area
+    circle_perc_tot_work = circle_derbs_area_tot/all_poly.area*100
+    derbs_perc_tot_work = circle_derbs_area_tot/derbs_poly.area*100
+    radius = 0
+    while circle_perc_tot_work >= 95:
+        radius = radius + 1
+        circle_derbs_area, circle_poly = draw_circle(rand_point,radius)
+        derbs_perc_tot_work = ((circle_derbs_area_tot + circle_derbs_area - 
+                           all_poly.intersection(circle_poly)
+                           .intersection(derbs_poly).area)/
+            derbs_poly.area*100)
+        circle_perc_tot_work = ((circle_derbs_area_tot + circle_derbs_area - 
+                            all_poly.intersection(circle_poly)
+                           .intersection(derbs_poly).area)/
+            (all_poly.area + circle_poly.area - 
+             all_poly.intersection(circle_poly).area)*100)
+        print(radius)
+        print(derbs_perc_tot_work)
+        print(circle_perc_tot_work)
+    
+    if radius > 1:
+        dict_derbs = {'lat': rand_point.y, 'long': rand_point.x, 'radius': radius-1}
+        df_derbs = df_derbs.append(dict_derbs, ignore_index = True)    
+        circle_poly = draw_circle(rand_point,radius - 1)[1]      
+        all_poly = cascaded_union([all_poly,circle_poly])
+        derbs_perc_tot = all_poly.intersection(derbs_poly).area/derbs_poly.area*100
+
+all_geoj = folium.GeoJson(data=all_poly,
+                          style_function=lambda x: {'fillColor': 'blue'})
+all_geoj.add_to(m)
+m.save("mymap.html")
 
