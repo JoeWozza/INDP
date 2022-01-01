251cdfeda795b02f6d234ec6b383ab10ff4b9e7c
Joe Wozniczka-Wells <Joe.WozniczkaWells@phe.gov.uk>
2022-01-01 17:43:39 +0000

    Changed 'until' date, added some audit bits at the end

--- Code/vaccine_scrape.py
+++ Code/vaccine_scrape.py
@@ -19,65 +19,34 @@ def datespan(startDate, endDate, delta):
         yield currentDate
         currentDate += delta
 
-# Use vaccination-related search terms from Bonnevie et al. plus some from
-# Sattar & Arifuzzaman
-
-searchTerms = [
-        # Bonnevie et al.
-        'antivaxmovement','antivaxxmovement','fuckvaccines','getvaccinated',
-               'GlobalVaccineSummit','learntherisk','NoForcedVaccination',
-               'StopForcedVaccination','StopMandatoryVaccination','vaccinateus',
-               'vaccinechoice','vaccinedamage','vaccinedeath','vaccinefailure',
-               'vaccineinjury','vaccineprotection','VaccinesCauseAIDs',
-               'VaccinesCauseAutism','VaccinesCauseRegressiveAutism',
-               'vaccinescausesids','vaccinesdangers','vaccineskill',
-               'vaccineskillandmaim','Vaccinesuncovered','vaccineswork',
-               'vaccinetruth','VaXXedII','anti-flu-vaccine','anti-vacc',
-               'anti-vaccination','anti-vaccine','anti-vaccines','anti-vax',
-               'anti-vaxer','anti-vaxers','antivaccination','antivaccine',
-               'antivaccines','antivax','antivaxer','antivaxers','antivaxx','antivaxxer','antivaxxers','flu shot',
-               'immunization','immunizations','pre-vax','pro-immunization',
-               'pro-immunizations','pro-vaccination','pro-vaccine',
-               'pro-vaccines','pro-vax','pro-vaxer','pro-vaxers','pro-vaxx',
-               'pro-vaxxer','pro-vaxxers','provaccine','provaccines',
-               'unvaccinated','unvax','unvaxed','unvaxxed','vacc damage','vacc schedule',
-               "vacc'd","vacc'n",'vaccinate','vaccinater','vaccinates',
-               'vaccinateing','vaccination','vaccinations','vaccine','vaccines',
-               'vaccine-hesitant','vacine','vacines','vaxer','VaXism','vaxx',
-               'vaxxed','vaxxer',
-               # Sattar & Arifuzzaman
-               'pfizer','Pfizer-BioNTech','BioNTechpfizer','Moderna',
-               'moderna_tx','Moderna-NIAID','NIAID','NIAID-Moderna',
-               'Johnson & Johnson','Johnson and Johnson','Janssen',
-               'Janseen Pharmaceutical','J&J','OXFORDVACCINE',
-               'Oxford-AstraZeneca','OxfordAstraZeneca','AstraZeneca',
-               'Vaxzevria','Covishield','Sputnik V','sputnikv','sputnikvaccine',
-               'covaxin','BharatBiotech','coronavac','sinovac',
-               # Me
-               'booster','first dose','second dose','third dose']
-
-searchTerms = ['vaccination','vaccine']
-
 # Read in df_utlas from csv (for now, will do this all within Python eventually)
-df_utlas = pd.read_csv("df_utlas.csv")
-df_utlas_ = df_utlas[df_utlas.utla.isin(['Nottingham','Derby'])]
-#df_utlas_ = df_utlas[df_utlas.utla.isin(['Derby'])]
+df_utlas = pd.read_csv("df_utlas_90.csv")
+
+#%% For real
 
 # Set since and until for entire period
 since = '2020-03-11'
-until = '2021-12-25'
+until = '2021-12-21'
 
-# Do some investigation to see whether doing it for a longer period misses some tweets
+searchTerms = ['vaccines','vaccine','Johnson and Johnson','vaccinated',
+               'vaccination','booster','pfizer','J&J','vaccinations',
+               'unvaccinated']
+
+# Read in df_utlas from csv (for now, will do this all within Python eventually)
+#df_utlas = pd.read_csv("df_utlas.csv")
+#df_utlas_ = df_utlas[df_utlas.utla.isin(['Nottingham','Derby'])]
+#df_utlas_ = df_utlas[df_utlas.utla.isin(['Derbyshire'])]
 
 # Create empty dataframes
 df_tweets = pd.DataFrame()
 df_error = pd.DataFrame()
 
+start = datetime.now()
 # Loop through UTLAs
-for utla in df_utlas_.drop_duplicates(subset=['utla']).utla:
+for utla in df_utlas.drop_duplicates(subset=['utla']).utla:
     
     # Loop through circles
-    df_utla = df_utlas[df_utlas.utla == utla]#.head(3)
+    df_utla = df_utlas[df_utlas.utla == utla]#.head(5)
     
     for c,(lat,long,radius) in enumerate(zip(df_utla.lat, df_utla.long, 
           df_utla.radius)):
@@ -110,6 +79,8 @@ for utla in df_utlas_.drop_duplicates(subset=['utla']).utla:
                         print('error')
                     else:
                         df['utla'] = utla
+                        df['term'] = term
+                        df['circle'] = c
                         df_tweets = df_tweets.append(df)
                         print('success')
                         print('')
@@ -118,48 +89,52 @@ for utla in df_utlas_.drop_duplicates(subset=['utla']).utla:
 # Dedupe tweets
 df_tweets_deduped = df_tweets.drop_duplicates(subset=['url','utla'])
 pd.value_counts(df_tweets_deduped.utla)
-# The above should work but sometimes gets the 'Unable to find guest token' error
-# Try tweepy instead
-    
-#%%
-import tweepy
-import numpy as np
+end = datetime.now()
 
-# Keys from here: https://developer.twitter.com/en/portal/projects/1475494865567899649/apps/new
-consumer_key = 'OPqZPaFbJHx8sXf8y7C5umylY'
-consumer_secret = 'qE6FdosS3aiSHNDcamEhxcGCxl40k4oKoPNoVTrrW8IMWkYSkB'
-# See here for keeping them secure: https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api
+df_tweets.to_csv('df_tweets_90.csv')
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+pd.value_counts(df_tweets.utla)
+pd.value_counts(df_tweets.term)
+pd.value_counts(df_tweets_deduped.utla)
+pd.value_counts(df_tweets_deduped.term)
 
-api = tweepy.API(auth)
+# Remove tweets that don't contain the term (these must be ones where the
+# user profile contains the term instead)
 
 
-df_tweets = pd.DataFrame(columns = ['utla','tweet_id','tweet_text',
-                                    'tweet_date','tweet_place','tweet_coords',
-                                    'user_name','user_location']
-                )
-for tweet in tweepy.Cursor(api.search_tweets, q='vaccine', 
-                           geocode='{0},{1},{2}km'.format(lat,long,radius),
-                           since="2021-03-11",until='2021-12-25',
-                           tweet_mode='extended').items(99999999999):
-    #print(tweet)
-    print(tweet.created_at)
-    #print(tweet.id)
-    #print(tweet.full_text)
-    #print(tweet.place)
-    #print(tweet.coordinates)
-    #print(tweet.user.name)
-    #print(tweet.user.location)
-    #print('')
-    dict_tweet = {'utla': utla, 'tweet_id': tweet.id, 
-                  'tweet_text': tweet.full_text, 
-                  'tweet_date': tweet.created_at, 'tweet_place': tweet.place, 
-                  'tweet_coords': tweet.coordinates, 
-                  'user_name': tweet.user.name, 
-                  'user_location': tweet.user.location}
-    df_tweets = df_tweets.append(dict_tweet, ignore_index=True)
 
-# Twitter API only returns data for the previous 7 days
+df_tweets_deduped_term = (df_tweets_deduped[df_tweets_deduped['content'].str
+                                            .lower().str.contains('|'
+                                                  .join(searchTerms))]
+                                            )
 
+df_tweets_deduped['lower'] = df_tweets_deduped['content'].str.lower()
 
+df_tweets_deduped.to_csv('df_tweets_deduped.csv')
+
+df_tweets_deduped.iloc[583]['lower']
+
+df_tweets_deduped_noterm = (df_tweets_deduped[~df_tweets_deduped['content'].str
+                                            .lower().str.contains('|'
+                                                  .join(searchTerms))]
+                                            )
+
+pd.value_counts(df_tweets_deduped_noterm.term)
+
+df__ = df_tweets_deduped_noterm[df_tweets_deduped_noterm['term']=='vaccines']
+# Both have accents above e
+df__ = df_tweets_deduped_noterm[df_tweets_deduped_noterm['term']=='J&J']
+
+df_tweets_deduped_term = (df_tweets_deduped[~df_tweets_deduped['content'].str.
+                                            contains(df_tweets_deduped['term'])
+                                            ])
+
+df__ = df_tweets_deduped_term[df_tweets_deduped_term['term']=='Johnson and Johnson']
+
+pd.value_counts(df_tweets_deduped_term.term)
+# Turns out there are no actual 'J&J' tweets and only 70 'Johnson and Johnson'
+# ones. Most/all of the Johnson and Johnson ones are about Boris Johnson, not
+# Johnson and Johnson. Revisit search terms.
+
+# For now output df_tweets_deduped_term to csv to use later
+df_tweets_deduped_term.to_csv('df_tweets_deduped_term.csv')
