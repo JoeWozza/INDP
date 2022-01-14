ddeb984b1b2de3d09fe2c593140e25440c6c951a
Joe Wozniczka-Wells <Joe.WozniczkaWells@phe.gov.uk>
2022-01-14 08:41:49 +0000

    Modularised twitter scrape code and put functions in class in separate script

--- Code/vaccine_scrape_tweepy.py
+++ Code/vaccine_scrape_tweepy.py
@@ -6,12 +6,19 @@ Created on Fri Jan  7 12:01:14 2022
 """
 #%% Packages
 
+from os import chdir, getcwd
+
+# Set file path
+filepath = ("C:\\Users\\Joe.WozniczkaWells\\Documents\\Apprenticeship\\UoB\\"
+            "SPFINDP21T4\\")
+chdir(filepath)
+
+from INDP.Code import TweetScrape
+class_ts = TweetScrape.TweetScrape()
+
 import pandas as pd
 import tweepy
 from datetime import datetime
-
-# Set file path and create log file for task scheduler
-filepath = "C:\\Users\\Joe.WozniczkaWells\\Documents\\Apprenticeship\\UoB\\SPFINDP21T4\\"
 
 #%% Twitter API access stuff
 
@@ -25,135 +32,18 @@ consumer_secret = 'qE6FdosS3aiSHNDcamEhxcGCxl40k4oKoPNoVTrrW8IMWkYSkB'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
-#%% Define functions
-
-def tweepy_scrape(api,df,df_timings,term,lat,long,radius,area,area_circle):
-    # Uses tweepy's api.search_tweets function to scrape tweets and add them to
-    # an existing dataframe, df. The function outputs df and a separate
-    # dataframe, df_timings, that captures information on the volumes of tweets 
-    # downloaded and time taken.
-    # api: valid tweepy.api.API object
-    # df: name of dataframe to contain tweets
-    # df_timings: name of dataframe to contain timings
-    # term: search term to be used to find tweets
-    # lat: latitude of circle centroid to be used to find tweets
-    # long: longitude of circle centroid to be used to find tweets
-    # radius: radius of circle centroid to be used to find tweets
-    # area: name of geographical area
-    # area_circle: name/number of geographical area circle
-    
-    print(area + ': circle ' + area_circle + ' - ' + term)
-    
-    term_start = datetime.now()
-    # Collect tweets in df_tweets
-    tweets = 0
-    for tweet in tweepy.Cursor(api.search_tweets, q=term, 
-                               geocode='{0},{1},{2}km'.format(lat,long,radius),
-                               tweet_mode='extended').items(999999999):
-        dict_tweet = {'area': area, 'area_circle': area_circle, 
-                      'search_term': term,
-                      'tweet_url': 'twitter.com/'+tweet.user.screen_name+
-                          '/status/'+tweet.id_str,
-                      'tweet_id': tweet.id,
-                      'tweet_text': tweet.full_text,
-                      'tweet_datetime': tweet.created_at,
-                      'tweet_place': tweet.place,
-                      'tweet_coords': tweet.coordinates,
-                      'tweet_language': tweet.lang,
-                      'tweet_likes': tweet.favorite_count,
-                      'tweet_retweets': tweet.retweet_count,
-                      'user_name': tweet.user.screen_name,
-                      'user_location': tweet.user.location,
-                      'user_followers': tweet.user.followers_count,
-                      'user_protected': tweet.user.protected,
-                      'user_tweets': tweet.user.statuses_count,
-                      'user_likes': tweet.user.favourites_count,
-                      'user_bio': tweet.user.description}
-        df = df.append(dict_tweet, ignore_index=True)
-        tweets += 1
-    
-    #Record timings
-    term_end = datetime.now()
-    time_taken = term_end - term_start
-    dict_timings = {'area': area, 'area_circle': area_circle, 
-                    'search_term': term, 'no_tweets': tweets,
-                    'time_taken': str(time_taken)}
-    df_timings = df_timings.append(dict_timings, ignore_index=True)
-    print(str(time_taken))
-    
-    return df, df_timings
-
-def check_circle(check_term,lat,long,radius):
-    # Checks whether the geographical circle contains any tweets containing a
-    # user-defined search term.
-    # check_term: search term used to check whether tweets exist
-    # lat: latitude of circle centroid to be used to find tweets
-    # long: longitude of circle centroid to be used to find tweets
-    # radius: radius of circle centroid to be used to find tweets
-    circ_tweets = 0
-    for tweet in tweepy.Cursor(api.search_tweets, q = check_term, 
-                               geocode='{0},{1},{2}km'.format(lat,long,
-                                        radius)
-                               ).items(1):
-        circ_tweets += 1
-        if circ_tweets >= 1:
-            circ_tweets = True
-        else:
-            circ_tweets = False
-    return circ_tweets
-
-def circles_scrape(df_area,df_tweets,df_timings):
-    # Loops through defined circles that make up an area and downloads tweets
-    # df_area: dataframe containing circles to loop through
-    # df_tweets: existing dataframe to append tweets to
-    # df_timings: existing dataframe to append timings to
-    
-    # Loop through circles    
-    for c,(lat,long,radius) in enumerate(zip(df_utla.lat, df_utla.long, 
-          df_utla.radius)):
-        
-        # Are there any tweets from the circle? Test using search term 
-        # 'twitter'.
-        circ_tweets = check_circle('twitter',lat,long,radius)
-        
-        # If there are no tweets in the circle, skip it, otherwise search for
-        # tweets.
-        if circ_tweets == False:
-            print('Skip circle ' + str(c+1) + ' in ' + area)
-        else:
-            # Loop through search terms
-            for term in searchTerms:
-                df_tweets, df_timings = tweepy_scrape(api,df_tweets,df_timings,
-                                                      term,lat,long,radius,
-                                                      area,str(c+1))
-
-    # Create date field from datetime
-    df_tweets['tweet_date'] = df_tweets.tweet_datetime.dt.date
-    
-    return df_tweets, df_timings
-
-def manual_assign_areas(df_in,area):
-    # Assigns tweets to an area based on whether user_location contains the
-    # area name
-    # df_in: dataframe containing tweets
-    # area: name of area to assign tweets to
-    df_temp = df_in[df_in.user_location.str.contains(area)].copy()
-    df_temp.area = area
-    
-    return df_temp
-
 #%% Data retrieval
 
 # Read in df_areas from csv (for now, may do this all within Python eventually)
 df_utlas = pd.read_csv("{0}df_utlas_90.csv".format(filepath))
-df_utlas = df_utlas[df_utlas.utla.isin(['Leicestershire','Derby'])]
+#df_utlas = df_utlas[df_utlas.utla.isin(['Derby','Nottingham'])]
 
 searchTerms = ['vaccines','vaccine','vaccinated',
                'vaccination','booster','pfizer',
                'vaccinations','unvaccinated',
                'astrazenica','antivaxxers',
                'vaccinate','vax','vaxxed']
-searchTerms = ['vaccines']
+#searchTerms = ['vaccines','vaccine']
 
 # Initiate dataframes to collect tweets and record timings
 df_tweets = pd.DataFrame()
@@ -161,11 +51,11 @@ df_timings = pd.DataFrame()
 
 start = datetime.now()
 # Loop through UTLAs
-for area in df_utlas.drop_duplicates(subset=['utla']).utla:
-    
-    df_utla = df_utlas[df_utlas['utla'] == area]#.head(5)
-    
-    df_tweets, df_timings = circles_scrape(df_utla,df_tweets,df_timings)
+utlas = df_utlas.drop_duplicates(subset=['utla']).utla
+
+df_tweets, df_timings = class_ts.circles_scrape_areas(api,utlas,df_utlas,
+                                                      'utla',df_tweets,
+                                                      df_timings,searchTerms)
 
 # Whole Midlands
 # Initiate dataframe to collect tweets and record timings
@@ -173,22 +63,24 @@ df_mids_tweets = pd.DataFrame()
 df_mids_timings = pd.DataFrame()
 
 # Download tweets    
-for term in searchTerms:
-    df_mids_tweets, df_mids_timings = tweepy_scrape(api,df_mids_tweets,
-                                                    df_mids_timings,term,
-                                                    52.8052096,-1.3846729,150,
-                                                    'Midlands','Midlands')
-    
-    # Create date field from datetime
-    df_mids_tweets['tweet_date'] = df_mids_tweets.tweet_datetime.dt.date
+df_mids_tweets, df_mids_timings = class_ts.tweepy_scrape_terms(api,
+                                                               df_mids_tweets,
+                                                               df_mids_timings,
+                                                               searchTerms,
+                                                               52.8052096,
+                                                               -1.3846729,150,
+                                                               'Midlands',
+                                                               'Midlands')
+
+# Create date field from datetime
+df_mids_tweets['tweet_date'] = df_mids_tweets.tweet_datetime.dt.date
 
 end = datetime.now()
 print('Overall: ' + str(end-start))
 
 # Distribute Midlands circle tweets to UTLAs using user_location
-for area in df_utlas.drop_duplicates(subset=['utla']).utla:
-    print(area)
-    df_tweets = df_tweets.append(manual_assign_areas(df_mids_tweets,area))
+df_tweets = df_tweets.append(class_ts.manual_assign_areas(df_mids_tweets,
+                                                          utlas))
 
 # Deduplicate by tweet_id and utla
 df_tweets_deduped = df_tweets.drop_duplicates(subset=['tweet_id','area'])
