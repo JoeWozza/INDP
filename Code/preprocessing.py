0b9645c677589d7242f549ad4a5dbe8aa76b7793
Joe Wozniczka-Wells <Joe.WozniczkaWells@phe.gov.uk>
2022-01-23 12:35:38 +0000

    Exploration of different lexicon-based SA techniques

--- Code/preprocessing.py
+++ Code/preprocessing.py
@@ -5,6 +5,13 @@ Created on Wed Dec 29 18:41:37 2021
 @author: Joe.WozniczkaWells
 """
 
+from os import chdir, getcwd,listdir
+
+# Set file path
+filepath = ("C:\\Users\\Joe.WozniczkaWells\\Documents\\Apprenticeship\\UoB\\"
+            "SPFINDP21T4\\INDP\\Data\\")
+chdir(filepath)
+
 import pandas as pd
 import re
 import emotlib
@@ -31,8 +38,22 @@ nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
 
+files = listdir()
+
+filestring = 'df_tweets_deduped_tweepy_'
+    
+tweets_files = [s for s in files if filestring in s]
+
+df_tweets = pd.DataFrame()
+
+for f in tweets_files:
+    print(f)
+    df_tweets = df_tweets.append(pd.read_csv(f))
+
+df_tweets_deduped = df_tweets.drop_duplicates(subset=['tweet_id','area'])
+
 #df = pd.read_csv('df_tweets_example.csv')
-df = pd.read_csv('df_tweets_deduped_term.csv')
+#df = pd.read_csv('df_tweets_deduped_term.csv')
 
 # 1) Initial cleaning: removal of URLs, Twitter handles, punctuation, numbers,
 #   single characters and multiple spaces, and conversion of emoticons to 
@@ -135,7 +156,7 @@ def lemmatise_incstop(text):
 
 #df['content_lemma_incstop'] = df['content_tokenized'].apply(lemmatise_incstop)
 
-df['content_lemma_incstop'] = df['content'].apply(clean).apply(tokenize).apply(lemmatise_incstop)
+df_tweets_deduped['content_lemma_incstop'] = df_tweets_deduped['tweet_text'].apply(clean).apply(tokenize).apply(lemmatise_incstop)
 
 # 6)	Identify negation: identifying words that negate the meaning of other 
 #   words (e.g. the piano is not heavy) is important and can prevent sentiment 
@@ -190,10 +211,129 @@ def sentconf(text):
             conf = 0
         return conf
 
-df['sentiment'] = df['content_lemma_incstop'].apply(VADERsentiment)
-df['sentconf'] = df['content_lemma_incstop'].apply(sentconf)
+df_tweets_deduped['sentiment'] = df_tweets_deduped['content_lemma_incstop'].apply(VADERsentiment)
+df_tweets_deduped['sentconf'] = df_tweets_deduped['content_lemma_incstop'].apply(sentconf)
 
-#%%
+df_utla_sent = df_tweets_deduped.groupby(['area'])[['sentiment','sentconf']].mean()
+
+# Scatter plot of sentiment v sentconf
+import matplotlib.pyplot as plt
+
+plt.scatter(df_tweets_deduped['sentiment'],df_tweets_deduped['sentconf'])
+
+# Density plots of sentiment by utla
+import seaborn as sns
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Birmingham']['sentiment'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottingham']['sentiment'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottinghamshire']['sentiment'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derby']['sentiment'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derbyshire']['sentiment'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Rutland']['sentiment'], cut=0)
+
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Birmingham']['sentconf'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottingham']['sentconf'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottinghamshire']['sentconf'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derby']['sentconf'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derbyshire']['sentconf'], cut=0)
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Rutland']['sentconf'], cut=0)
+
+#%% Try TextBlob instead of VADER
+
+from textblob import TextBlob
+
+def textblobsentiment(text):
+    return TextBlob(text).sentiment.polarity
+
+df_tweets_deduped['sentiment_tb'] = df_tweets_deduped['content_lemma_incstop'].apply(textblobsentiment)
+
+# Scatter plot of VADER v TextBlob sentiment
+plt.scatter(df_tweets_deduped['sentiment'],df_tweets_deduped['sentiment_tb'])
+
+df_utla_sent = df_tweets_deduped.groupby(['area'])[['sentiment','sentconf','sentiment_tb']].mean()
+
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Birmingham']['sentiment_tb'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottingham']['sentiment_tb'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottinghamshire']['sentiment_tb'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derby']['sentiment_tb'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derbyshire']['sentiment_tb'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Rutland']['sentiment_tb'])
+
+# TextBlob produces much narrower distributions
+
+#%% Try SentiWordNet
+
+# Pos tag data
+
+pos_dict = {'J':wordnet.ADJ, 'V':wordnet.VERB, 'N':wordnet.NOUN, 'R':wordnet.ADV}
+def token_stop_pos(text):
+    tags = pos_tag(word_tokenize(text))
+    newlist = []
+    for word, tag in tags:
+        if word.lower() not in set(stopwords.words('english')):
+            newlist.append(tuple([word, pos_dict.get(tag[0])]))
+    return newlist
+
+df_tweets_deduped['POS tagged'] = df_tweets_deduped['tweet_text'].apply(clean).apply(token_stop_pos)
+df_tweets_deduped[['tweet_text','POS tagged']].head()
+
+nltk.download('sentiwordnet')
+from nltk.corpus import sentiwordnet as swn
+
+wordnet_lemmatizer = WordNetLemmatizer()
+def sentiwordnetanalysis(pos_data):
+    sentiment = 0
+    tokens_count = 0
+    for word, pos in pos_data:
+        if not pos:
+            continue
+        lemma = wordnet_lemmatizer.lemmatize(word, pos=pos)
+        if not lemma:
+            continue
+        synsets = wordnet.synsets(lemma, pos=pos)
+        if not synsets:
+            continue
+        # Take the first sense, the most common
+        synset = synsets[0]
+        swn_synset = swn.senti_synset(synset.name())
+        sentiment += swn_synset.pos_score() - swn_synset.neg_score()
+        tokens_count += 1
+        # print(swn_synset.pos_score(),swn_synset.neg_score(),swn_synset.obj_score())
+    return sentiment
+    #if not tokens_count:
+    #    return 0
+    #if sentiment>0:
+    #    return "Positive"
+    #if sentiment==0:
+    #    return "Neutral"
+    #else:
+    #    return "Negative"
+
+df_tweets_deduped['sentiment_swn'] = df_tweets_deduped['POS tagged'].apply(sentiwordnetanalysis)
+df_tweets_deduped[['tweet_text','sentiment_swn','sentiment']].head()
+
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Birmingham']['sentiment_swn'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottingham']['sentiment_swn'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Nottinghamshire']['sentiment_swn'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derby']['sentiment_swn'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Derbyshire']['sentiment_swn'])
+sns.kdeplot(df_tweets_deduped[df_tweets_deduped['area']=='Rutland']['sentiment_swn'])
+
+# Distributions also seem much narrower than VADER
+
+# Scatter plot of VADER v SWN sentiment
+plt.scatter(df_tweets_deduped['sentiment'],df_tweets_deduped['sentiment_swn'])
+
+sns.kdeplot(data = df_tweets_deduped, x = 'sentiment', cut=0)
+sns.kdeplot(data = df_tweets_deduped, x = 'sentiment_tb', cut=0)
+sns.kdeplot(data = df_tweets_deduped, x = 'sentiment_swn', cut=0)
+
+#%% Experimenting with VADER
+
+
+analyzer.polarity_scores('1st')
+analyzer.polarity_scores('first')
+analyzer.polarity_scores('2nd')
+analyzer.polarity_scores('second')
 
 analyzer.polarity_scores('think tweet Vaccine rolling_on_the_floor_laughing rolling_on_the_floor_laughing')
 analyzer.polarity_scores('🤣')
@@ -330,5 +470,20 @@ analyzer.polarity_scores(lemmatise(['kind normality']))
 pd.value_counts(df[df['utla']=='Derby'].username)
 
 df_booster = df[df['term'] == 'booster']
+
+#%%
+
+import nltk
+nltk.download('sentiwordnet')
+
+from nltk.corpus import sentiwordnet as swn
+
+list(swn.senti_synsets('happy'))
+
+test = swn.senti_synset('happy.a.01')
+
+test.pos_score()
+test.neg_score()
+test.obj_score()
 
 
