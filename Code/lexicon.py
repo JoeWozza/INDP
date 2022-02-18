ce38921149ecadc63287d3630cdd07a0bedf7e6b
Joe Wozniczka-Wells <joe.wozniczkawells@phe.gov.uk>
2022-02-18 12:55:15 +0000

    Tidy up, comments etc.

--- Code/lexicon.py
+++ Code/lexicon.py
@@ -15,33 +15,30 @@ chdir(filepath)
 import pandas as pd
 from INDP.Code import VADER
 class_v = VADER.VADER()
-    
+
 #%% Read in data
 
-# Create input
+# Define common string at start of file names
 filestring_eng = 'df_tweets_eng_tweepy_'
-
+# Get list of all files in path
 files = listdir(filepath + "\\INDP\\Data")
-
+# Get list of all files containing common string
 tweets_files_eng = [s for s in files if filestring_eng in s]
-
+# Combine data into single dataframe
 df_tweets_eng = pd.DataFrame()
-
 for f in tweets_files_eng:
     print(f)
     df_tweets_eng = df_tweets_eng.append(pd.read_csv("INDP\\Data\\" + f))
-
-df_tweets_deduped_eng = df_tweets_eng.drop_duplicates(subset=['tweet_id'])
-df_in = df_tweets_deduped_eng[['tweet_text','tweet_id']]
-df_in['tweet_id'] = df_in['tweet_id'].astype('Int64').apply(str)
+# Deduplicate by tweet_id
+df_VADER = df_tweets_eng.drop_duplicates(subset=['tweet_id']).reset_index().drop(columns=['index','Unnamed: 0'])
 
 #%% VADER
 
+# Load packages
 import pandas as pd
 import re
 import emoji
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
 import nltk
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
@@ -54,17 +51,18 @@ nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 analyzer = SentimentIntensityAnalyzer()
 
+# Define stopwords list
 stopwords_fin = class_v.fin_stopwords(stopwords.words('english'))
-
-# Process data
-df_VADER = df_tweets_deduped_eng.copy()
+# Sort out tweet_id field
 df_VADER['tweet_id'] = df_VADER['tweet_id'].astype('Int64').apply(str)
+# Clean, tokenize and lemmatise tweet content
 df_VADER['content_lemma'] = (df_VADER['tweet_text'].apply(class_v.clean)
     .apply(class_v.tokenize)
     .apply(class_v.lemmatise, stopwords_list=stopwords_fin))
+# Calculate sentiment
 df_VADER['sentiment'] = df_VADER['content_lemma'].apply(class_v.VADERsentiment)
+# Calculate sentiment confidence
 df_VADER['sentconf'] = df_VADER['content_lemma'].apply(class_v.sentconf)
 
-# This takes a few minutes, whereas LRSentiA takes hours.
-
+# Output results to csv
 df_VADER.to_csv("INDP//Data//df_VADER.csv")
