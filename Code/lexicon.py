9e18c8b2ecba80a5df7a3efc0abfb7b36259b856
Joe Wozniczka-Wells <joe.wozniczkawells@phe.gov.uk>
2022-02-18 12:36:16 +0000

    Removed problem example from Code/lexicon.py

--- Code/lexicon.py
+++ Code/lexicon.py
@@ -58,7 +58,7 @@ analyzer = SentimentIntensityAnalyzer()
 stopwords_fin = class_v.fin_stopwords(stopwords.words('english'))
 
 # Process data
-df_VADER = df_tweets_deduped_eng.head(10).copy()
+df_VADER = df_tweets_deduped_eng.copy()
 df_VADER['tweet_id'] = df_VADER['tweet_id'].astype('Int64').apply(str)
 df_VADER['content_lemma'] = (df_VADER['tweet_text'].apply(class_v.clean)
     .apply(class_v.tokenize)
@@ -69,36 +69,3 @@ df_VADER['sentconf'] = df_VADER['content_lemma'].apply(class_v.sentconf)
 # This takes a few minutes, whereas LRSentiA takes hours.
 
 df_VADER.to_csv("INDP//Data//df_VADER.csv")
-
-# This example is a problem. Compound output is positive, for some reason, but
-# all pairs analysed by sentconf are either negative or neutral, so confidence
-# score = 1. Removing stop words helps, because it produces positive and
-# negative pairs, but this seems like a fluke.
-text = "This CAN NOT be ignore The government must acknowledge this issue and either prove it invalid or act to halt vaccine for male in this age group"
-analyzer.polarity_scores(text)
-
-sentconf(text)
-
-text_ = word_tokenize(text)
-text2 = ''
-for ele in text_:
-    if ele not in stopwords_list:
-        text2 = text2 + ' ' + ele
-
-analyzer.polarity_scores(text2)
-sentconf(text2)
-
-stopwords_list = stopwords.words('english')
-print(len(stopwords_list))
-stopwords_list.remove("wasn't")
-print(len(stopwords_list))
-
-# Remove some words from stopwords, where they have an impact on the sentiment
-remove_list = []
-for word in stopwords.words('english'):
-    if analyzer.polarity_scores(word + " good")['compound'] != analyzer.polarity_scores("good")['compound']:
-        remove_list.append(word)
-
-stopwords_list = [ele for ele in stopwords.words('english') if ele not in remove_list]
-print(len(stopwords_list))
-# PUT THIS (^) IN EARLIER ON TO REMOVE STOP WORDS DURING LEMMATISATION
