fcd7d731d424f80bd81f69c066f3d9c1396c4e70
Joe Wozniczka-Wells <Joe.WozniczkaWells@phe.gov.uk>
2022-03-01 10:07:23 +0000

    Tidied up where data is read in from and visualisations are saved to

--- Code/lex_audit.py
+++ Code/lex_audit.py
@@ -18,13 +18,23 @@ filepath = ("C:\\Users\\Joe.WozniczkaWells\\Documents\\Apprenticeship\\UoB\\"
 chdir(filepath)
 
 import pandas as pd
+import os
+
+# Create folder in which to save visualisations
+images_folder = '{0}/INDP/Images'.format(filepath)
+audit_folder = '{0}/INDP/Images/VADER_audit'.format(filepath)
+
+if not os.path.exists(images_folder):
+    os.makedirs(images_folder)
+if not os.path.exists(audit_folder):
+    os.makedirs(audit_folder)
     
 #%% Read in data
 
 # Create input
 filestring_eng = 'df_tweets_eng_tweepy_'
 
-files = listdir(filepath + "\\INDP\\Data")
+files = listdir("INDP/Data/Tweets")
 
 tweets_files_eng = [s for s in files if filestring_eng in s]
 
@@ -32,7 +42,7 @@ df_tweets_eng = pd.DataFrame()
 
 for f in tweets_files_eng:
     print(f)
-    df_tweets_eng = df_tweets_eng.append(pd.read_csv("INDP\\Data\\" + f))
+    df_tweets_eng = df_tweets_eng.append(pd.read_csv("INDP/Data/Tweets/{0}".format(f)))
 
 #pd.set_option('display.float_format', lambda x: '%.30f' % x)
 df_tweets_eng['tweet_id'] = df_tweets_eng['tweet_id'].astype('Int64').apply(str)
@@ -40,7 +50,7 @@ df_tweets_eng['tweet_id'] = df_tweets_eng['tweet_id'].astype('Int64').apply(str)
 df_tweets_deduped_eng = df_tweets_eng.drop_duplicates(subset=['tweet_id','search_term'])
 df_tweets_deduped_eng = df_tweets_deduped_eng.set_index('tweet_id')
 
-df_VADER = pd.read_csv("INDP//Data//df_VADER.csv")
+df_VADER = pd.read_csv("INDP/Data/VADER/df_VADER.csv")
 
 # Categorise sentiment
 def cat_sentiment(row):
@@ -55,7 +65,7 @@ df_VADER['sentiment_cat'] = df_VADER.apply(lambda row: cat_sentiment(row), axis=
 
 # Join onto other columns
 df_VADER['tweet_id'] = df_VADER['tweet_id'].astype('Int64').apply(str)
-df_VADER = df_VADER.set_index('tweet_id')[['sentiment','sentiment_cat']]
+df_VADER = df_VADER.set_index('tweet_id')[['sentiment','sentiment_cat','sentconf']]
 df_VADER = df_VADER.join(df_tweets_deduped_eng, how = 'right', lsuffix='_l',
                          rsuffix='_r')
 
@@ -70,7 +80,7 @@ fig, ax = plt.subplots(figsize = (12,6))
 fig = sns.distplot(df_VADER_unique['sentiment'], ax=ax, color='#007C91', kde_kws={'clip': (-1,1)})                
 ax.set(xlabel = 'VADER sentiment score')
 plt.tight_layout()
-plt.savefig("INDP//Images//VADER_sentiment.png")
+plt.savefig("{0}/VADER_sentiment.png".format(audit_folder))
 
 # Plot distribution of sentiment confidence scores
 fig, ax = plt.subplots(figsize = (12,6))
@@ -78,7 +88,7 @@ fig, ax = plt.subplots(figsize = (12,6))
 fig = sns.distplot(df_VADER_unique['sentconf'], ax=ax, color='#007C91', kde_kws={'clip': (0,1)})               
 ax.set(xlabel = 'Confidence in VADER sentiment score')
 plt.tight_layout()
-plt.savefig("INDP//Images//VADER_sentconf.png")
+plt.savefig("{0}/VADER_sentconf.png".format(audit_folder))
 
 # Plot distribution of sentiment scores by search term
 g = sns.FacetGrid(df_VADER, col = 'search_term', col_wrap = 5)
@@ -87,7 +97,7 @@ g.map(sns.histplot, 'sentiment', color='#007C91', kde=True,
 g.set_axis_labels(x_var = 'VADER sentiment score', y_var = 'Density')
 g.set_titles(col_template = '{col_name}')
 g.tight_layout()
-g.savefig("INDP//Images//VADER_sentiment_search_term.png")
+g.savefig("{0}/VADER_sentiment_search_term.png".format(audit_folder))
 
 # Plot distribution of sentiment scores by search term
 g = sns.FacetGrid(df_VADER, col = 'search_term', col_wrap = 5)
@@ -97,7 +107,7 @@ g.set_axis_labels(x_var = 'Confidence in VADER sentiment score',
                   y_var = 'Density')
 g.set_titles(col_template = '{col_name}')
 g.tight_layout()
-g.savefig("INDP//Images//VADER_sentconf_search_term.png")
+g.savefig("{0}/VADER_sentconf_search_term.png".format(audit_folder))
 
 # Scatter plot of sentiment score v confidence score
 fig, ax = plt.subplots(figsize = (12,6))
@@ -106,7 +116,7 @@ fig = sns.scatterplot(data=df_VADER_unique, y='sentconf', x='sentiment', ax=ax,
 ax.set(ylabel = 'Confidence in VADER sentiment score', 
        xlabel = 'VADER sentiment score')
 plt.tight_layout()
-plt.savefig("INDP//Images//VADER_sentiment_sentconf.png")
+plt.savefig("{0}/VADER_sentiment_sentconf.png".format(audit_folder))
 
 # Word clouds by sentiment_cat
 # Wordcloud with stop words excluded
@@ -128,7 +138,7 @@ for cat in df_VADER_unique.drop_duplicates(['sentiment_cat'])['sentiment_cat'].t
     plt.figure()
     plt.imshow(wordcloud_excstopwords)
     plt.axis("off")
-    plt.savefig('INDP/Images/VADER_wordcloud_excstopwords_{0}.png'.format(cat))
+    plt.savefig('{0}/VADER_wordcloud_excstopwords_{1}.png'.format(audit_folder,cat))
 
 
 
